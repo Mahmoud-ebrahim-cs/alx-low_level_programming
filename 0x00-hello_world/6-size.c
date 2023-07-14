739629b7061ec62e93f031b7775ae49d1c4ff476
Mahmoud-ebrahim-cs <e17377572>
2023-07-14 00:55:37 -0700

    a

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * min - Entry point
+ *
+ * Description: A C Program that print size of datatype with size function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of n int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+return (0);
+}
